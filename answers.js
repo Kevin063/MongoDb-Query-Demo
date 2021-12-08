6fc049453e4d60865aa5f59ff7c4111963201757
Kevin063 <562910950@qq.com>
2021-12-08 14:54:11 -0500

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,1 +1,2 @@
-db.restaurants.find({name:"Caffe Dante"},{restaurent_id:1})
+db.restaurants.find({name:"Caffe Dante"},{restaurent_id:1});
+db.restaurants.find({name:{$regex:"Steak"}},{restaurent_id:1,name:1});
